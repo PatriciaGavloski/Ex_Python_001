fb84cc9e13aef5c4373cb009d1fab7f358268bb9
Patricia Novaski Gavloski <128609847+PatriciaGavloski@users.noreply.github.com>
2023-05-18 09:44:54 -0300

    Add files via upload
    Exercícios realizados com o curso de Python do canal do youtube cursos em vídeo

---
+++ aula09.py
@@ -0,0 +1,5 @@
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite mais um número: '))
+s = n1 + n2
+print('A soma dos valores', n1, 'e', n2, 'vale',s)
+print('A Soma entre {} e {} vale {}'.format(n1, n2, s))
